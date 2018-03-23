f9e365f781f852c9c55d0c2aac21ebf295af6909
s.shimada <s.shimada@tlega.co.jp>
2018-03-23 18:59:27 +0900

    Mod ログ修正.

--- elevator.js
+++ elevator.js
@@ -40,14 +40,13 @@
           });
           // エレベータが床に到着したときにトリガされます。
           elevator.on("stopped_at_floor", function(floorNum) {
-              console.log("stopped_at_floor #" + elevator.elevatorNum);
-              console.log("waitQueue #" + elevator.elevatorNum + ": " + floors.waitQueue);
+              console.log("stopped_at_floor #" + elevator.elevatorNum + " floorNum: " + floorNum);
               floors.removeWaitQueue(floorNum);
               console.log("waitQueue #" + elevator.elevatorNum + ": " + floors.waitQueue);
           });
           // 乗客がエレベータ内のボタンを押したときにトリガされます。
           elevator.on("floor_button_pressed", function(floorNum) {
-              console.log("floor_button_pressed #" + elevator.elevatorNum);
+              console.log("floor_button_pressed #" + elevator.elevatorNum + "floorNum: " + floorNum);
               elevator.goToFloor(floorNum);
               console.log("destinationQueue #" + elevator.elevatorNum + ": " + elevator.destinationQueue);
           });
